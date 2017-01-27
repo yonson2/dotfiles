2c6c014bab059f5d74457d65225dbaa1c3901cf0
Peter S <s@pramos.me>
2017-01-27 18:00:13 +0100

    feat(fish): add config for fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+# alias
+alias vim "nvim"
+alias gm "git commit -m "
+alias gs "git status"
+alias ga "git add "
+
+# double-bang (like in bash)
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
