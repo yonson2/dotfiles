d850143bbcaba1e05cf6e1611484ff2963dde458
Peter S <s@pramos.me>
2021-02-10 20:17:19 +0100

    clean master repo in favour of branches per device config

--- .config/fish/functions/pr.fish
+++
@@ -1,5 +0,0 @@
-function pr
-  set repo (git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/]\(.*\)\.git.*/\1/")
-  set branch (git name-rev --name-only HEAD)
-  xdg-open https://github.com/$repo/pull/new/$branch
-end
